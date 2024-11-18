d8412a2c94d2cd0826421d45a9a7941d9d488b38
lumen nganje <lumennganje592@gmail.com>
2024-11-18 09:23:26 +0100

    header complete and dark mode added

---
+++ src/components/Header.tsx
@@ -0,0 +1,30 @@
+import Link from 'next/link'
+import { FaBars, FaSearch } from 'react-icons/fa'
+import { ModeToggle } from './mode-toggle'
+
+const Header = () => {
+  return (
+    <header className="shadow-sm bg-gray-50 py-4 px-6 md:px-16 flex justify-between items-center dark:bg-black">
+        <Link href="/">
+            <span className='inline-block p-3 max-tn:p-2 rounded-md bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-white'>ShareIt</span>
+        </Link>
+        <form className=' h-10 pr-2 rounded-sm shadow-sm bg-white flex items-center max-tn:hidden'>
+            <input type='search' placeholder='Search' className='h-10  rounded-sm px-2 py-2 text-black bg-white focus:outline-none'/>
+             <FaSearch className=''/>
+        </form>
+        <ul className=' h-10 flex justify-between items-center gap-2 max-sm:hidden max-md:text-sm'>
+            <li><Link href="" className='py-2 px-3 hover:text-blue-400 text-blue-600'>Home</Link></li>
+            <li><Link href="" className='py-2 px-3 hover:text-blue-400 text-blue-600'>About</Link></li>
+            <li><Link href="" className='py-2 px-3 hover:text-blue-400 text-blue-600'>Projects</Link></li>
+        </ul>
+        <div className='h-10 flex items-center gap-3 max-md:text-sm'>
+            <ModeToggle />
+            <Link href="" className='p-3 hover:text-blue-400 text-blue-600 max-x-tn:hidden'>Sign up</Link>
+            <FaBars className='w-6 h-4 text-blue-600 hover:cursor-pointer sm:hidden' />
+        </div>
+        
+    </header>
+  )
+}
+
+export default Header
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,69 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+  	extend: {
+  		colors: {
+  			background: 'var(--background)',
+  			foreground: 'var(--foreground)'
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		},
+  		keyframes: {
+  			'accordion-down': {
+  				from: {
+  					height: '0'
+  				},
+  				to: {
+  					height: 'var(--radix-accordion-content-height)'
+  				}
+  			},
+  			'accordion-up': {
+  				from: {
+  					height: 'var(--radix-accordion-content-height)'
+  				},
+  				to: {
+  					height: '0'
+  				}
+  			}
+  		},
+  		animation: {
+  			'accordion-down': 'accordion-down 0.2s ease-out',
+  			'accordion-up': 'accordion-up 0.2s ease-out'
+  		}
+  	},
+	screens:{
+		'x-tn': '280px',
+		 // => @media (min-width: 280px) { ... }
+
+		'tn': '500px',
+		 // => @media (min-width: 455px) { ... }
+
+		'sm': '690px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '790px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
 export default config;
